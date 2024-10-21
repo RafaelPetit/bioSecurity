9315280dcad630d32a35f02e360dd72ad56c690a
Rafael Petit <rpetit.dev@gmail.com>
2024-10-21 15:21:35 -0300

    feat: testes iniciais

---
+++ src/test/java/bio/security/api/domain/user/UserControllerTest.java
@@ -0,0 +1,88 @@
+package bio.security.api.domain.user;
+
+import bio.security.api.domain.user.dto.CreateUserDto;
+import bio.security.api.domain.user.dto.ResponseCreateUserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserRepository repository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when creating a user with invalid data")
+    @WithMockUser(roles = "ADMIN")
+    void createUserWithInvalidData() throws Exception {
+        CreateUserDto createUserDto = new CreateUserDto(0, "", "", null);
+
+        MvcResult result = mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createUserDto)))
+                .andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+
+    @Test
+    @DisplayName("Should return 403 when creating a user without admin role")
+    @WithMockUser(roles = "USER")
+    void createUserWithoutAdminRole() throws Exception {
+        CreateUserDto createUserDto = new CreateUserDto(2, "username", "Teste123@!", Role.ADMIN);
+
+        MvcResult result = mockMvc.perform(post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createUserDto)))
+                .andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+}
